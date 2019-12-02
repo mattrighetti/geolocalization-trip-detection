1d73ed4c3860fa548c7df9d7b02bde4accca685b
luca.ferrera <luca.ferrera@mia-platform.eu>
2019-12-02 18:33:53 +0100

    WIP

--- app.py
+++ app.py
@@ -20,4 +20,4 @@ def healtz():
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=os.environ['HTTP_PORT'] debug=True)
+    app.run(host='0.0.0.0', port=os.environ['HTTP_PORT'], debug=True)
