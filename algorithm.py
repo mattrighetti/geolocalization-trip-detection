2da9ceb4ac09b1073ff741f91c65f7939a1ba636
Luca Terracciano <luca.terracciano@mail.polimi.it>
2019-12-06 19:35:24 +0100

    Added contracts for each function

---
+++ algorithm.py
@@ -0,0 +1,62 @@
+# TODO Step 0 Parse the GeoJSON.
+#       Input: Flask request.args.get()
+#       Output: user_route -> List of Point (Shapely class)
+user_route = []
+
+# TODO Step 1 Find the first(I) and the last(F) user coordinates.
+#       Input: user_route: a list of Point()
+#       Output: 2 Point -> I and F
+
+#   Find the first user coordinates.
+#   params:  - user_route: a list of Point()
+#   returns: - the first point of the user route
+def find_first(user_route):
+
+#   Find the last user coordinates.
+#   params:  - user_route: a list of Point()
+#   returns: - the last point of the user route
+def find_last(user_route):
+
+# TODO Step 2 Find bus stops near I. Do the same for E.
+#       Input: I and F
+#       Output: 2 list Tuples: Ilist, Flist -> tuple: (bus, stop)
+
+
+# TODO Step 3 Do the intersection in order to find the bus lines in common.
+#       Input: Ilist, Flist
+#       Output: Ilist, Flist -> filtered with only the tuples (bus, stop) with bus field in I and E
+
+
+# TODO Step 4 Create a list of bus routes that have a starting point in Ilist and an end in Flist
+#       Input: Ilist, Flist
+#       Output: bus_routes -> list of list of Point -> every list of Point represents a bus route
+
+
+# TODO Step 5 For every route in bus_route compute its metrics.
+#
+#       metrics: 1) user_coordinates_matched: number of Point in user_route contained in at least
+#                                             one polygon of the bus route.
+#                2) polygons_matched: number of polygons of the bus route containing at least one 
+#                                     Point of user_route.
+#
+#       Input: bus_routes, user_route
+#       Output: list of route_dictionary { 
+#                                          "route" : bus_route (the list of Point), 
+#                                          "user_coordinates_matched": user_coordinates_matched, 
+#                                          "polygons_matched": polygons_matched
+#                                        }
+
+
+# TODO Step 6 Search the dictionary with the maximum metrics (TODO Decide a criterion to choose the best one)
+#       Input: list of route_dictionary
+#       Output: route_dictionary
+
+
+# TODO Step 7 Calculate the distance with haversine()
+#       Input: route_dictionary
+#       Output: km_travelled
+
+
+# TODO Step 8 Save the data in the database
+#       Input: user_id, ticket_id, km_travelled
+
